4661e36d13fe948e959cc6a574575b3aa603eb65
Dũng Bùi <dungpro96bn@gmail.com>
2024-08-05 16:26:30 +0700

    style homepage

--- wp-content/themes/monopp-navi/404.php
+++ wp-content/themes/monopp-navi/404.php
@@ -1,10 +1,24 @@
 <?php get_header(); ?>
 
-<div id="404" class="404">
-  <div class="not-found-box">
-    <p class="not-found-text">申し訳ございません。<br>お探しのページは見つかりませんでした。</p>
-    <a href="<?php echo do_shortcode('[homePath]'); ?>" class="not-found-button">トップページへ戻る</a>
-  </div>
-</div><!-- #404 -->
+    <div id="breadcrumb" class="breadcrumb">
+        <div class="inner">
+            <ol>
+                <li>
+                    <a href="<?php echo home_url(); ?>">TOP</a></i>
+                </li>
+                <li>404 not found</li>
+            </ol>
+        </div>
+    </div>
+
+    <div id="page-404" class="">
+        <div class="inner">
+            <div class="not-found-box">
+                <p class="not-found-text">申し訳ございません。<br>お探しのページは見つかりませんでした。</p>
+                <a href="<?php echo do_shortcode('[homePath]'); ?>" class="not-found-button">トップページへ戻る</a>
+            </div>
+        </div>
+
+    </div><!-- #404 -->
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/monopp-navi/taxonomy-post-tags.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+<?php
+$category = get_queried_object();
+if ($category && isset($category->name)) {
+    $category_name = $category->name;
+}
+?>
+
+    <div id="breadcrumb" class="breadcrumb">
+        <div class="inner">
+            <ol>
+                <li>
+                    <a href="<?php echo home_url(); ?>">TOP</a></i>
+                </li>
+                <li><?php echo get_queried_object()->name; ?></li>
+            </ol>
+        </div>
+    </div>
+
+    <div id="archive" class="new-container">
+        <div class="related-article">
+            <div class="inner">
+                <h2 class="heading"><?php echo $category_name; ?></h2>
+                <?php if (have_posts()) : ?>
+                    <ul class="related-article-list">
+                        <?php
+                        while (have_posts()) : the_post();
+                            ?>
+                            <li class="article-item">
+                                <a class="link-post" href="<?php the_permalink(); ?>">
+                                    <p class="image-post">
+                                        <img src="<?php echo get_the_post_thumbnail_url(); ?>">
+                                    </p>
+                                    <h2 class="title-post"><?php echo get_the_title(); ?></h2>
+                                </a>
+                                <div class="info-bottom">
+                                    <div class="category">
+                                        <?php
+                                        $country_lists = wp_get_post_terms($post->ID, 'post-tags', array("fields" => "all"));
+                                        foreach($country_lists as $country_list) { ?>
+                                            <a href="<?php echo get_category_link($country_list->term_id); ?>"># <?php echo $country_list->name; ?></a>
+                                        <?php } ?>
+                                    </div>
+                                    <p class="date-time number"><?php echo get_the_date(); ?></p>
+                                </div>
+                            </li>
+                        <?php endwhile; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        </div>
+
+    </div><!-- #new -->
+
+<?php get_footer(); ?>
