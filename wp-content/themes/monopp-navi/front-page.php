fe7bbad8cabab6294c4f03d53ac18b2bef06ae44
David RiddellNet <david+du@riddellnet.com>
2024-09-10 10:59:15 +0700

    change title on the homepage

--- wp-content/themes/monopp-navi/front-page.php
+++ wp-content/themes/monopp-navi/front-page.php
@@ -94,7 +94,7 @@
         <div class="inner">
             <div class="new-article">
                 <div class="new-article-list">
-                    <h2 class="heading-block"><span>特集記事</span></h2>
+                    <h2 class="heading-block"><span>新着記事</span></h2>
                     <ul class="article-list">
                         <?php
                         $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
